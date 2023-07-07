f7da268bb79fb3e81f1d7bb139f3e8cfba7b2009
Veronika <nikakras.dance@gmail.com>
2023-07-07 18:33:41 +0300

    task 1

---
+++ src/js/task-01.js
@@ -0,0 +1,13 @@
+const categoriesList = document.querySelector('#categories');
+
+const categoriesItems = categoriesList.querySelectorAll('li.item');
+
+console.log('Number of categories:', categoriesItems.length)
+
+
+categoriesItems.forEach((item) => {
+    const categoryTitle = item.querySelector('h2').textContent;
+    const categoryElements = item.querySelectorAll('li');
+    console.log('Category:', categoryTitle);
+    console.log('Elements: ', categoryElements.length);
+});
