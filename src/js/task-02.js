a612d20f899ee8f632ab89f90b391e576b99cd01
Veronika <nikakras.dance@gmail.com>
2023-07-09 20:21:56 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const itemEl = document.createElement('li');
+itemEl.classList.add('item');
+itemEl.textContent = 'Potatoes'
+console.log(itemEl);
+
+
+const listUl = document.querySelector('#categories');
